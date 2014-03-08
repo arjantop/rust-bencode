c34e6803460cc2f8758aeb1ca359c5631c688c3d
Arjan Topolovec <arjan.top@gmail.com>
2014-03-08 19:07:30 +0100

    Added license (Apache 2.0 and MIT)

--- src/bencode.rs
+++ src/bencode.rs
@@ -1,8 +1,18 @@
+// Copyright 2014 Arjan Topolovec
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #[crate_id = "bencode"];
+#[license = "MIT/ASL2"];
 #[crate_type = "rlib"];
 #[crate_type = "dylib"];
 #[deny(warnings)];
 #[feature(macro_rules)];
+
 extern crate collections;
 extern crate serialize;
 
